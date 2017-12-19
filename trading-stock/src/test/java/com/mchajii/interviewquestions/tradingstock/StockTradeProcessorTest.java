03f358f96c8d14c16628976c2b839396b9aae74b
mchadji <mohamed.chadji@fednot.be>
2017-12-19 20:40:03 +0100

    Added test cases about computation of max profit

--- trading-stock/src/test/java/com/mchajii/interviewquestions/tradingstock/StockTradeProcessorTest.java
+++ trading-stock/src/test/java/com/mchajii/interviewquestions/tradingstock/StockTradeProcessorTest.java
@@ -3,27 +3,34 @@ package com.mchajii.interviewquestions.tradingstock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-
 /**
  * @author Mohamed Chajii
  */
-public class StockTradeProcessorTest {
+class StockTradeProcessorTest {
 
-    // TODO: Finish tests.
+	@Test
+	void shouldThrowExceptionWhenStockPricesIsNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(null));
+	}
 
-    @Test
-    public void shouldThrowExceptionWhenStockPricesIsNull() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(null));
-    }
+	@Test
+	void shouldThrowExceptionWhenStockPricesIsEmpty() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(new int[]{}));
+	}
 
-    @Test
-    public void shouldThrowExceptionWhenStockPricesIsEmpty() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(Collections.emptyList()));
-    }
+	@Test
+	void shouldThrowExceptionWhenStockPricesSizeIsLessThanTwo() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(new int[]{1}));
+	}
 
-    @Test
-    public void shouldThrowExceptionWhenStockPricesSizeIsLessThanTwo() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> StockTradeProcessor.getMaxProfit(Collections.singletonList(1)));
-    }
+	@Test
+	void shouldReturnMaxProfit() {
+		Assertions.assertAll("Compute max profit",
+				() -> Assertions.assertEquals(StockTradeProcessor.getMaxProfit(new int[]{10, 7, 5, 8, 11, 9}), 6), // random array of stock prices
+                () -> Assertions.assertEquals(StockTradeProcessor.getMaxProfit(new int[]{10, 2}), -8), // we can have a negative profit
+                () -> Assertions.assertEquals(StockTradeProcessor.getMaxProfit(new int[]{5, 7, 8, 9, 10, 11}), 6), // stock prices sorted in ascending order
+				() -> Assertions.assertEquals(StockTradeProcessor.getMaxProfit(new int[]{10, 10, 10}), 0), // stock price does not change
+				() -> Assertions.assertEquals(StockTradeProcessor.getMaxProfit(new int[]{11, 10, 9, 8, 7, 5}), -1) // stock prices sorted in descending order
+		);
+	}
 }
