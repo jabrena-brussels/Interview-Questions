bbb239e1e8bc4f5c209dee07f45fc4da34345322
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-02-16 21:51:57 +0000

    Refactor the initial implementation

--- sumTwoNumbers/src/main/java/org/trares/challenges/sumTwoNumbers/SummableImpl.java
+++ sumTwoNumbers/src/main/java/org/trares/challenges/sumTwoNumbers/SummableImpl.java
@@ -4,26 +4,30 @@ import java.util.LinkedList;
 
 public class SummableImpl implements Summable {
 
+    private int getParam(final LinkedList<Integer> param) {
+
+        final StringBuilder stringBuilder = new StringBuilder();
+        param.stream().forEach((digit)-> {
+            stringBuilder.append(digit);
+        });
+        return Integer.parseInt(stringBuilder.toString());
+    }
+
+    private LinkedList<Integer> convertFromLinkedListToInt(final int result){
+
+        final LinkedList<Integer> linkedListResult = new LinkedList<>();
+        linkedListResult.add(result);
+
+        return linkedListResult;
+    }
+
     @Override
     public LinkedList<Integer> sumTwoNumbers(final LinkedList<Integer> l1, final LinkedList<Integer> l2) {
 
-        final StringBuilder stringBuilder = new StringBuilder();
-        l1.stream().forEach((digit)-> {
-            stringBuilder.append(digit);
-        });
-        final int param1 = Integer.parseInt(stringBuilder.toString());
-
-        final StringBuilder stringBuilder2 = new StringBuilder();
-        l2.stream().forEach((digit)-> {
-            stringBuilder2.append(digit);
-        });
-        final int param2 = Integer.parseInt(stringBuilder2.toString());
-
+        final int param1 = getParam(l1);
+        final int param2 = getParam(l2);
         final int sum = param1 + param2;
 
-        final LinkedList<Integer> linkedListResult = new LinkedList<>();
-        linkedListResult.add(sum);
-
-        return linkedListResult;
+        return convertFromLinkedListToInt(sum);
     }
 }
