0c53660adddea35e3eed619006abd2d4b7ac3dc9
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-02-16 21:26:27 +0000

    Adding first Test

---
+++ sumTwoNumbers/src/main/java/org/trares/challenges/sumTwoNumbers/Summable.java
@@ -0,0 +1,9 @@
+package org.trares.challenges.sumTwoNumbers;
+
+import java.util.LinkedList;
+
+public interface Summable {
+
+    LinkedList<Integer> sumTwoNumbers(LinkedList<Integer> l1, LinkedList<Integer> l2);
+
+}
--- sumTwoNumbers/src/test/java/org/trares/challenges/sumTwoNumbers/SummableTest.java
+++ sumTwoNumbers/src/test/java/org/trares/challenges/sumTwoNumbers/SummableTest.java
@@ -1,9 +1,33 @@
 package org.trares.challenges.sumTwoNumbers;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.LinkedList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 @RunWith(JUnit4.class)
 public class SummableTest {
+
+    @Test
+    public void sumTwoNumbersWithSuccessTest() {
+
+        final LinkedList<Integer> linkedlistParam1 = new LinkedList<Integer>();
+        linkedlistParam1.add(1);
+
+        final LinkedList<Integer> linkedlistParam2 = new LinkedList<Integer>();
+        linkedlistParam2.add(1);
+
+        final Summable summable = new SummableImpl();
+        final LinkedList linkedListResult = summable.sumTwoNumbers(linkedlistParam1, linkedlistParam2);
+
+        final LinkedList<Integer> linkedlistExpectedResult = new LinkedList<Integer>();
+        linkedlistParam1.add(2);
+
+        assertThat(linkedlistExpectedResult, is(linkedListResult));
+    }
 
 }
