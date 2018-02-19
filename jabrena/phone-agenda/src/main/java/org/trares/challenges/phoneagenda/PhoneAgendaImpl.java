46ca8de88346d23224416ab4ff2971c6a3f07b97
jabrena-brussels <bren@juanantonio.info>
2018-02-19 23:16:47 +0000

    Reviewing the idea about BinarySearch

--- jabrena/phone-agenda/src/main/java/org/trares/challenges/phoneagenda/PhoneAgendaImpl.java
+++ jabrena/phone-agenda/src/main/java/org/trares/challenges/phoneagenda/PhoneAgendaImpl.java
@@ -22,6 +22,15 @@ public class PhoneAgendaImpl implements PhoneAgenda {
 
         int index = 0;
 
+        index = linealSearch();
+
+        return index;
+    }
+
+    private int linealSearch() {
+
+        int index = 0;
+
         if(partyList.length == 1){
             return index;
         }
@@ -29,17 +38,50 @@ public class PhoneAgendaImpl implements PhoneAgenda {
         for(int x = 0; x < partyList.length -1; x++){
 
             String current = partyList[x];
-            String next = partyList[x+1];
+            String next = partyList[x + 1];
 
             LOGGER.trace("Comparison result: {}", current.compareTo(next));
 
             if(current.compareTo(next) > 0) {
-                index = x+1;
+                index = x + 1;
                 break;
             }
-
         }
 
         return index;
     }
+
+    //Development in progress
+    //Original Idea: https://github.com/eugenp/tutorials/blob/master/algorithms/src/main/java/com/baeldung/algorithms/binarysearch/BinarySearch.java
+    private int iterativeBinarySearch() {
+
+        int index = Integer.MAX_VALUE;
+
+        int low = 0;
+        int high = partyList.length - 1;
+        int mid;
+
+        while (low <= high) {
+
+            mid = (low + high) / 2;
+
+            String current = partyList[mid];
+            String next = partyList[mid + 1];
+
+            /*
+            Criterias to move
+
+            if (partyList[mid] < key) {
+                low = mid + 1;
+            } else if (partyList[mid] > key) {
+                high = mid - 1;
+            } else if (partyList[mid] == key) {
+                index = mid;
+                break;
+            }
+            */
+        }
+        return index;
+    }
+
 }
